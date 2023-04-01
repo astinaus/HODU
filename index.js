ae33f6699ca3b778314b0d18e74725f16e265648
astinaus <slaayers0@gmail.com>
2023-04-01 09:43:24 +0900

    modal 완성

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
 const navbar = document.getElementById("navbar");
+const btnModal = document.getElementById("btn-modal");
+const closeModal = document.getElementById("modal-close");
+const modal = document.getElementById("modal");
 
 openMenu.addEventListener("click", () => {
     navbar.style.display = "block";
@@ -8,4 +11,19 @@ openMenu.addEventListener("click", () => {
 
 closeMenu.addEventListener("click", () => {
     navbar.style.display = "none";
+});
+
+btnModal.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("modal-wrapper")) {
+        modal.style.display = "none";
+    }
 });
